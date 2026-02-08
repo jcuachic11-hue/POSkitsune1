6bb8cfeaea0d00c63b6df7d9c15a6b1b7d8dded7
jean <j.cuachic11@gmail.com>
2026-02-08 00:25:33 -0600

    arreglo del index.js

--- scr/index.js
+++ scr/index.js
@@ -30,6 +30,6 @@ app.use('/ventas', ventas);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
