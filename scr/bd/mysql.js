b00a41cf37918fb4acb7a2418b6b5e7ebe86d7e1
jean <j.cuachic11@gmail.com>
2026-02-07 23:53:21 -0600

    segunod add

--- scr/bd/mysql.js
+++ scr/bd/mysql.js
@@ -3,10 +3,12 @@ const mysql = require('mysql2/promise');
 
 // Configuración de la conexión
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',          // tu usuario de MySQL
-    password: '', // tu contraseña de MySQL
-    database: 'kitsune'   // tu base de datos
+     host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306
+
 });
 
 // Exporta directamente el pool
